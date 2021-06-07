81297addf1599658b0eed7d6e03694832931410a
Ernst Dzeravianka <ernst.dzeravianka@softensity.com>
2021-06-07 17:57:52 +0300

    setup initial test and added flag to run correctly "npx playwright debug"

--- tests/gcp.spec.js
+++ tests/gcp.spec.js
@@ -1,6 +1,41 @@
-import { chromium } from 'playwright';
-import expect from 'expect';
+const { chromium } = require("playwright");
+
 let browser;
 let page;
 
-describe()
+describe("Test Suite",()=>{
+
+    beforeAll(async ()=>{
+        browser = await chromium.launch({ headless: false });
+        const context = await browser.newContext();
+        page = await context.newPage();
+        await page.goto("https://cloud.google.com/");
+    })
+
+    afterAll(async () =>{
+        if (!page.isClosed()) {
+            browser.close();
+        }
+    })
+
+    test("Test Case", async ()=>{
+
+        await page.click('[placeholder="Search"]');
+        await page.fill('[placeholder="Search"]', 'calculator');
+        await page.press('[placeholder="Search"]', 'Enter');
+        await page.click('text=Google Cloud Platform Pricing Calculator');
+        await page.waitForSelector("devsite-iframe iframe");
+        const handle = await page.$('devsite-iframe iframe');
+        const body = await handle.contentFrame();
+
+        await body.waitForTimeout(10000);
+        await body.waitForSelector("iframe#myFrame");
+
+        const handleCalc = await body.$("iframe#myFrame");
+        const calcBody = await handleCalc.contentFrame();
+
+        await calcBody.waitForSelector("//*[@id='select_79']");
+        await calcBody.click("//*[@id='select_79']");
+
+    })
+})
