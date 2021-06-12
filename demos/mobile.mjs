37bbb97100c4a1444095fe06b82f44923c9d47f4
Ernst-D <80358262+Ernst-D@users.noreply.github.com>
2021-06-12 17:39:26 +0300

    "circleci-project-setup" merge (#1)

--- demos/mobile.mjs
+++ demos/mobile.mjs
@@ -8,6 +8,6 @@ const context =  await browser.newContext({
     ...devices["iPhone 11 Pro"]
 })
 const page = await context.newPage();
-await page.goto("https://www.whatismybrowser.com/detect/what-is-my-user-agent")
+await page.goto("https://www.google.by/search?q=what+is+my+user+agent&oq=what+is+my+user&sourceid=chrome&ie=UTF-8")
 await page.screenshot({ path: "example.png" });
 await browser.close();
