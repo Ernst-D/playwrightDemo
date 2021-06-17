42143f02037771e603f032485371afc693ada08d
Ernst Dzeravianka <ernst.dzeravianka@softensity.com>
2021-06-17 19:35:24 +0300

    handle click from list

--- tests/gcp.spec.js
+++ tests/gcp.spec.js
@@ -16,26 +16,26 @@ describe("Test Suite",()=>{
         browser = await chromium.launch({ headless: false });
         const context = await browser.newContext();
         page = await context.newPage();
-        gcpMain = new GcpMain(page);
-        await gcpMain.navigate("https://cloud.google.com")
-    })
+        gcpMain = new GcpMain(page);        
+        gcpCalc = new CloudCalculator(page);
+        gcpSearchResults = new SearchResults(page);
+        await gcpMain.navigate("https://cloud.google.com");
+        // let locs = (await page.$$("#select_container_79  md-option")).pop()
+    });
 
     afterAll(async () =>{
         if (!page.isClosed()) {
             browser.close();
         }
-    })
+    });
 
     test("GCP Cloud Calc", async () => {
-        await gcpMain.search("calculator")
-       
-        gcpSearchResults = new SearchResults(page);
-        await gcpSearchResults.page.click(gcpSearchResults.cloudCalcLink)
+        await gcpMain.search("calculator");
+        
+        await gcpSearchResults.page.click(gcpSearchResults.cloudCalcLink);
 
-        gcpCalc = new CloudCalculator(page);
         const calcBody = await gcpCalc.waitForCalcFrame();
-        await calcBody.waitForSelector("//*[@id='select_79']");
-        await calcBody.click("//*[@id='select_79']");
+        await gcpCalc.setUpInstance(calcBody);
 
-    })
-})
+    });
+});
