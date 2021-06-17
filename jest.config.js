ff6c08985a6dcf4badf38da1d67668c6f38027cf
Ernst Dzeravianka <ernst.dzeravianka@softensity.com>
2021-06-17 11:51:19 +0300

    added babel preset for playwright

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
     testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
     testPathIgnorePatterns: ["/node_modules/"],
     setupFilesAfterEnv: ["./jest.setup.js"],
+    preset: 'jest-playwright-preset',
   };
