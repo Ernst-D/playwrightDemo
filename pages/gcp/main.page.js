e25a37c75654d018de7353e84801a289c57dd6a3
Ernst-D <80358262+Ernst-D@users.noreply.github.com>
2021-06-21 09:45:28 +0300

    added some base mixins for pages (#3)

--- pages/gcp/main.page.js
+++ pages/gcp/main.page.js
@@ -1,18 +1,23 @@
+import { basePageActions } from "../../utils/mixins";
+
 class GcpMain {
     constructor(page = null) {
         /**
          * @typedef {Promise<Page>} Page in the browser context
          */
         this.page = page;
+        Object.assign(GcpMain.prototype,basePageActions)
     }
+
+    get searchInput() {return "[placeholder='Search']"}
 
     async navigate(url){ 
         await this.page.goto(url);  
     }
     async search (stringToSearch){
-        await this.page.click("[placeholder='Search']");
-        await this.page.fill("[placeholder='Search']", stringToSearch);
-        await this.page.press("[placeholder='Search']", "Enter");
+        await this.page.click(this.searchInput);
+        await this.page.fill(this.searchInput, stringToSearch);
+        await this.page.press(this.searchInput, "Enter");
     }
 
 }
