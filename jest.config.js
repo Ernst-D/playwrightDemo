a4f6dedc63a500204976b3c1439c76ed9c4f3859
ernstDzeravianka <ernst.dzeravianka@softensity.com>
2021-07-02 17:18:02 +0300

    added patternMatch module to jest config in order to avoid import error (it is written in es6)

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
     testPathIgnorePatterns: ["/node_modules/"],
     setupFilesAfterEnv: ["./jest.setup.js"],
-    preset: 'jest-playwright-preset',
+    preset: "jest-playwright-preset",
+    transformIgnorePatterns: [
+      "node_modules/(?!@tpoisseau)"
+    ],
   };
